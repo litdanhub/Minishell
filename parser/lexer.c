aad02c5e4854ddf13e096b99fd4f9e236e642c07
Dsalimov <dsalimov@student.42vienna.com>
2026-02-19 16:22:19 +0100

    updated lexer.c

--- parser/lexer.c
+++ parser/lexer.c
@@ -6,13 +6,13 @@
 /*   By: dsalimov <dsalimov@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 15:17:22 by dsalimov          #+#    #+#             */
-/*   Updated: 2026/02/19 15:34:20 by dsalimov         ###   ########.fr       */
+/*   Updated: 2026/02/19 16:22:07 by dsalimov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_print_tokens(t_data *data) //delete
+void	ft_print_tokens(t_data *data)//delete after use
 {
 	t_token *tmp;
 
